1c47d8a3caba558552652ee85700889fa68f0dca
Sierra <1969608+wackysysadmin@users.noreply.github.com>
2025-01-26 22:48:28 +0400

    Update http.py

--- libs/http.py
+++ libs/http.py
@@ -1,4 +1,4 @@
-from prometheus_client import generate_latest
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 from flask import Flask, Response, current_app
 import libs.prometheus
 
@@ -7,14 +7,13 @@ app = Flask(__name__)
 cash_gauge = libs.prometheus.cash_gauge()
 portfolio_gauge = libs.prometheus.portfolio_gauge()
 
-
 @app.route("/metrics")
 def metrics():
     api_key = current_app.config['API_KEY']
     libs.prometheus.set_cash_gauge(api_key, cash_gauge)
     libs.prometheus.set_portfolio_gauge(api_key, portfolio_gauge)
-    return Response(generate_latest(), mimetype='text/openmetrics')
-
+    # Use the older "text/plain; version=0.0.4" content type:
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 def run(api_key, prometheus_host, prometheus_port):
     app.config['API_KEY'] = api_key
